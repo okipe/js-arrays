eb573134b1542efe2636c3e177b4e67af87f0906
Oscar Roman <oscarroman.pe@gmail.com>
2023-08-07 18:19:57 -0500

    filter challenge

---
+++ server/03-filter-challenge.js
@@ -0,0 +1,26 @@
+function filterByLength(array) {
+    return array.filter(palabra => palabra.length >= 4)
+  };
+
+  
+/* INTRUCTIONS
+
+Tienes un array de strings que contienen diferentes palabras, tu reto es retornar un array solo con las que cumplan con la condición de tener 4 o más letras.
+
+Para solucionarlo vas a encontrar una función llamada filterByLength que recibe un parámetro de entrada:
+
+array: Un array de strigs con palabras
+Dentro del cuerpo de la función filterByLength debes escribir tu solución.
+
+Ejemplo 1:
+
+Input: ['amor', 'sol', 'piedra', 'día']
+Output: [ 'amor', 'piedra' ]
+
+Ejemplo 2:
+
+Input: ['rosa', 'gol', 'pez', 'día', 'gafas']
+Output: [ 'rosa', 'gafas' ]
+
+
+*/
